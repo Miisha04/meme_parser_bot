9fccada13fd4f62744c8ac53d6138a7eae8ec96c
m1chae11 <misha.gladkov.04@gmail.com>
2024-08-19 23:34:01 +0800

    black list

--- handlers.py
+++ handlers.py
@@ -13,6 +13,9 @@ from keyboards import main_kb
 router = Router()
 
 good_tokens = {}
+bad_tokens = []
+stop_event = asyncio.Event()
+bad_token_event = asyncio.Event()
 
 
 @router.message(CommandStart())
@@ -41,6 +44,16 @@ async def get_sol_price(message: Message):
         await message.answer("Failed to parse JSON data.")
 
 
+async def decrement_values_periodically():
+    while True:
+        await asyncio.sleep(300)  # Ждем 5 минут
+
+        for key in good_tokens:
+            good_tokens[key] -= 0.5
+
+        print("Values decremented by 0.5")
+
+
 async def subscribe_trades(ws, message):
 
     payload = {
@@ -59,9 +72,6 @@ async def unsubscribe_trades(ws, message):
     print("Unsubscribed from trades")
 
 
-stop_event = asyncio.Event()
-
-
 async def check_trades_logic(ws, message):
     stop_button = InlineKeyboardBuilder()
     stop_button.add(types.InlineKeyboardButton(
@@ -74,6 +84,8 @@ async def check_trades_logic(ws, message):
     )
 
     stop_event.clear()
+    bad_token_event.clear()
+    asyncio.create_task(decrement_values_periodically())
 
     while True:
         if stop_event.is_set():
@@ -87,53 +99,65 @@ async def check_trades_logic(ws, message):
             mint_address = data.get('Mint')
             sol_amount = data.get('SolAmount') / 1000000000
 
-            if sol_amount > 0.4:
-                if data.get('IsBuy'):
-                    if mint_address in good_tokens:
-                        good_tokens[mint_address] += sol_amount
+            if mint_address not in bad_tokens:
+                if sol_amount > 0.4:
+                    if data.get('IsBuy'):
+                        if mint_address in good_tokens:
+                            good_tokens[mint_address] += sol_amount
+                        else:
+                            good_tokens[mint_address] = sol_amount
+
+                        del_key = ""
+
+                        for key, value in good_tokens.items():
+                            if value > 20:
+                                token_text = get_data_from_pumpfun(f"https://frontend-api.pump.fun/coins/{key}")
+
+                                if token_text is not None:
+                                    token_data = json.loads(token_text)
+                                    del_key = key
+                                    trade_link = f"https://t.me/achilles_trojanbot?start=r-bankx0-{key}"
+
+                                    await message.answer(
+                                        f"Volume Surge: {round(value, 2)} SOL\n\n"
+                                        f"Token name: {token_data.get('name')} (${token_data.get('symbol')})\n"
+                                        f"Market Cap: ${round(token_data.get('usd_market_cap'), 0)}\n\n"
+                                        f"CA: <code>{key}</code>\n\n"
+                                        f"TG: {token_data.get('telegram')}\n"
+                                        f"Twitter: {token_data.get('twitter')}\n"
+                                        f"Website: {token_data.get('website')}\n\n"
+                                        f"Trade link: {trade_link}\n"
+                                        f"<a href='{token_data.get('image_uri')}'>IMG</a>",
+                                        parse_mode="HTML"
+                                    )
+
+                                    # Создаем кнопку для пометки токена как "плохого"
+                                    bad_token_button = InlineKeyboardBuilder()
+                                    bad_token_button.add(types.InlineKeyboardButton(
+                                        text="Fuck this",
+                                        callback_data=f"bad_token_{key}")
+                                    )
+                                    await message.answer(
+                                        "Нажми кнопку чтобы ебать токен",
+                                        reply_markup=bad_token_button.as_markup()
+                                    )
+
+                                else:
+                                    del_key = key
+                                    await message.answer(
+                                        f"Volume Surge: {round(value, 2)} SOL\n\n"
+                                        f"CA: <code>{key}</code>\n",
+                                        parse_mode="HTML"
+                                    )
+                            elif value < 0:
+                                del_key = key
+
+                        if del_key != "":
+                            del good_tokens[del_key]
+
                     else:
-                        good_tokens[mint_address] = sol_amount
-
-                    del_key = ""
-
-                    for key, value in good_tokens.items():
-                        if value > 15:
-
-                            token_text = get_data_from_pumpfun(f"https://frontend-api.pump.fun/coins/{key}")
-
-                            if token_text is not None:
-                                token_data = json.loads(token_text)
-                                del_key = key
-                                trade_link = f"https://t.me/achilles_trojanbot?start=r-bankx0-{key}"
-
-                                await message.answer(
-                                    f"Volume Surge: {round(value, 2)} SOL\n\n"
-                                    f"Token name: {token_data.get('name')} (${token_data.get('symbol')})\n"
-                                    f"Market Cap: ${round(token_data.get('usd_market_cap'), 0)}\n\n"
-                                    f"CA: <code>{key}</code>\n\n"
-                                    f"Trade link: {trade_link}\n\n"
-                                    f"TG: {token_data.get('telegram')}\n"
-                                    f"Twitter: {token_data.get('twitter')}\n"
-                                    f"Website: {token_data.get('website')}\n"
-                                    f"<a href='{token_data.get('image_uri')}'>IMG</a>",
-                                    parse_mode="HTML"
-                                )
-                            else:
-                                del_key = key
-                                await message.answer(
-                                    f"Volume Surge: {round(value, 2)} SOL\n\n"
-                                    f"CA: <code>{key}</code>\n",
-                                    parse_mode="HTML"
-                                )
-                        elif value < 0:
-                            del_key = key
-
-                    if del_key != "":
-                        del good_tokens[del_key]
-
-                else:
-                    if mint_address in good_tokens:
-                        good_tokens[mint_address] -= sol_amount
+                        if mint_address in good_tokens:
+                            good_tokens[mint_address] -= sol_amount
 
         except (websockets.ConnectionClosedError, websockets.ConnectionClosedOK) as e:
             print(f"Connection closed: {e}. Reconnecting...")
@@ -155,3 +179,17 @@ async def handle_stop(callback_query: types.CallbackQuery):
     stop_event.set()  # Устанавливаем событие, чтобы остановить цикл
     await callback_query.message.edit_reply_markup()  # Убираем кнопки
     await callback_query.answer("Останавливаю процесс...")
+
+
+@router.callback_query(lambda c: c.data.startswith("bad_token_"))
+async def handle_bad_token(callback_query: types.CallbackQuery):
+    token_id = callback_query.data.split("_")[-1]  # Получаем идентификатор токена
+    if token_id not in bad_tokens:
+        bad_tokens.append(token_id)
+        await callback_query.message.edit_reply_markup()
+        await callback_query.answer("Токен добавлен в список плохих")
+
+        for token in bad_tokens:
+            print(token)
+
+        print("\n")
