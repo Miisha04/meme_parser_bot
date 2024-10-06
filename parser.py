b5c9ac2e13d0ba0330fb7afc6525441e03aadb57
Michael <misha.gladkov.04@gmail.com>
2024-10-07 05:14:37 +0800

    fix pf parser

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import requests
 
-from config import login, password, port
+from config import LOGIN, PASSWORD, PROXY_HOST, PROXY_PORT
 
 
 # Заголовки с пользовательским агентом
@@ -9,17 +9,14 @@ headers = {
 }
 
 # Прокси-сервер
-proxies = {
-    'https': f'socks5://{login}:{password}@{port}'
-}
+proxy_url = f'socks5://{LOGIN}:{PASSWORD}@{PROXY_HOST}:{PROXY_PORT}'
 
 
 def get_data_from_pumpfun(url):
     try:
-        response = requests.get(url, headers=headers, proxies=proxies)
+        response = requests.get(url, headers=headers, proxies=proxy_url)
         response.raise_for_status()  # Проверка на ошибки HTTP
         return response.text  # Возвращаем текст ответа
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
-
